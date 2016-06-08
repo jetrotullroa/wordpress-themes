193f1c1e3367c782ec141414e0126607cad3ee57
jetrotullroa <jetrotullroa@gmail.com>
2016-06-08 16:44:12 +0800

    add function to add logo (first letter of sitename or image) and function to add background image on header. Remove header brand - title/description on singular post/page

--- themes/theme_scores/inc/custom-header.php
+++ themes/theme_scores/inc/custom-header.php
@@ -23,12 +23,19 @@
 function popperscores_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'popperscores_custom_header_args', array(
 		'default-image'          => '',
-		'default-text-color'     => '000000',
-		'width'                  => 1000,
-		'height'                 => 250,
+		'default-text-color'     => 'ffffff',
+		'width'                  => 1600,
+		'height'                 => 420,
 		'flex-height'            => true,
 		'wp-head-callback'       => 'popperscores_header_style',
 	) ) );
+
+	add_theme_support( 'custom-logo', array(
+		'width'       => 96,
+		'height'     	=> 96,
+		'flex-width' 	=> false,
+		'flex-height' => false,
+	) );
 }
 add_action( 'after_setup_theme', 'popperscores_custom_header_setup' );
 
